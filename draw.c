ac4925334e05181aaec1ab9461d96ce4e12d8a18
isabel42 <106957731+isabel42@users.noreply.github.com>
2023-03-30 15:39:45 +0200

    start end end_end

--- draw.c
+++ draw.c
@@ -6,7 +6,7 @@
 /*   By: itovar-n <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/29 20:14:31 by itovar-n          #+#    #+#             */
-/*   Updated: 2023/03/30 15:22:09 by itovar-n         ###   ########.fr       */
+/*   Updated: 2023/03/30 15:39:18 by itovar-n         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,22 +30,30 @@ void ft_printline(t_data_img *img, t_input *data)
 	int i;
 	int j;
 
-	t_iso start;
-	// t_iso end;
+	t_iso *start;
+	t_iso *end;
+	t_iso *end_end;
 	i = 0;
 	while(i < data->column)
 	{
 		j = 0;
 		while(j < data->row)
 		{
-			// end = *ft_iso((i + 1) + (20 * (i + 1)), j + (20 * j), data->map[j][i + 1]);
-			start = *ft_iso(i + (20 * i), j + (20 * j), data->map[j][i]);
 			if(i < data->column -1)
 			{
+				start = ft_iso(i + (20 * i), j + (20 * j), data->map[j][i]);
+				end = ft_iso((i + 1) + (20 * (i + 1)), j + (20 * j), data->map[j][i + 1]);
 				tracerSegment(img, i + (20 * i), j + (20 * j), (i + 1) + (20 * (i + 1)), j + (20 * j));
+				free(start);
+				free(end);
 			}
-			if(j < data->row -1 ) 
+			if(j < data->row -1 )
+			{
+				start = ft_iso(i + (20 * i), j + (20 * j), data->map[j][i]);
+				end_end = ft_iso(i + (20 * i), (j + 1) + (20 * (j + 1)), data->map[j][i + 1]);
 				tracerSegment(img, i + (20 * i), j + (20 * j), i + (20 * i), (j + 1) + (20 * (j + 1)));
+				free(start);
+			}
 			j++;
 		}
 		i++;
